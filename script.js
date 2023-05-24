b9d63c13bed1a5053194ef53613334ed9a49e57c
Isaac Summers <monogator@gmail.com>
2023-05-24 06:50:16 -0700

    Finalized Project

--- script.js
+++ script.js
@@ -7,9 +7,33 @@ container.style.width = '800px'
 document.body.appendChild(container);
 container.style.margin = 'auto';
 container.style.padding = '10px';
+container.style.border = '1px solid black';
+document.body.style.backgroundColor = '#FAF9F6';
+container.style.position = 'relative';
+container.style.backgroundColor = '#9a8275';
 
-makeRandomBoard();
 
+  
+//function to get a multicolored gradient over a checkerboard background
+function gradientSetter() {
+    //backgroundImage = linear-gradient(45deg, getRandomColor(), getRandomColor());
+    let gradient = document.createElement('div');
+    gradient.style.height = '800px';
+    gradient.style.width = '800px';
+    gradient.style.backgroundImage = `linear-gradient(to bottom right, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
+    gradient.style.opacity = '.5';
+    gradient.style.position = 'absolute'
+    
+    container.appendChild(gradient);
+    gradient.appendChild(checkerBoard());
+
+}
+
+
+checkerBoard();
+
+
+//Set of functions to make a randomly colored checkerboard
 function getRandomColor() {
     let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
     return randomColor;
@@ -29,41 +53,33 @@ function makeRandomBoard() {
 
 
 
-function makeRedSquare() {
+//Set of functions to make a red/black checkerboard
+function makeSquare(color) {
     let div = document.createElement('div');
     div.style.height = '12.5%';
     div.style.width = '12.5%';
-    div.style.backgroundColor = 'red'; 
+    div.style.backgroundColor = color; 
     
     container.appendChild(div);
 }
 
-function makeBlackSquare() {
-    let div = document.createElement('div');
-    div.style.height = '12.5%';
-    div.style.width = '12.5%';
-    div.style.backgroundColor = 'black'; 
-    
-    container.appendChild(div);
-}
-
-function makeCheckerboard1() {
+function row1() {
     for (let i = 0; i < 8; i++) {
         if (i % 2 === 0) {
-            makeBlackSquare();
+            makeSquare('#353535');
         } else {
-            makeRedSquare();
+            makeSquare('#cec9c6');
         }
     }
 }
 console.log(container);
 
-function makeCheckerboard2() {
+function row2() {
     for (let i = 0; i < 8; i++) {
         if (i % 2 === 0) {
-            makeRedSquare();
+            makeSquare('#cec9c6');
         } else {
-            makeBlackSquare();
+            makeSquare('#353535');
         }
     }
 }
@@ -71,9 +87,9 @@ function makeCheckerboard2() {
 function checkerBoard() {
     for (let i = 0; i < 8; i++) {
         if (i % 2 === 0) {
-            makeCheckerboard1();
+            row1();
         } else {
-            makeCheckerboard2();
+            row2();
         }
     }
 }
